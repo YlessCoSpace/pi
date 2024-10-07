38355ce0c54eafc3c0bd98bf8f7f6f8ae93430a3
Vivatsathorn Thitasirivit <vivatsathorn@outlook.co.th>
2024-10-08 01:13:56 +0700

    view

--- src/run_view.py
+++ src/run_view.py
@@ -8,13 +8,16 @@ if __name__ == '__main__':
         print(f'Error: Unable to open video stream')
         exit()
 
+    cv2.namedWindow('Image')
+    cv2.setWindowProperty('Image', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
     while True:
         ret, frame = cap.read()
         if not ret:
             print('Error: Unable to read frame from stream')
             break
 
-        cv2.imshow('Detection', frame)
+        cv2.imshow('Image', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
