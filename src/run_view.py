2986eaf360520496c2e298ccbb6a23995b18b61a
Vivatsathorn Thitasirivit <vivatsathorn@outlook.co.th>
2024-10-08 01:29:16 +0700

    view

--- src/run_view.py
+++ src/run_view.py
@@ -1,7 +1,12 @@
+import math
+
 import cv2
 from lib_network import *
 
 cap = find_network_cam(username='admin', password='admin')
+target_fps = 2
+target_fps = math.floor(target_fps)
+frame_interval = int(cap.get(cv2.CAP_PROP_FPS)) // target_fps
 
 if __name__ == '__main__':
     if not cap.isOpened():
@@ -10,14 +15,34 @@ if __name__ == '__main__':
 
     cv2.namedWindow('Image')
     cv2.setWindowProperty('Image', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    stopped = False
 
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            print('Error: Unable to read frame from stream')
-            break
+    try:
 
-        cv2.imshow('Image', frame)
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print('Error: Unable to read frame from stream')
+                break
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            cv2.imshow('Image', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                stopped = True
+
+            # for _ in range(1000 // target_fps):
+            #     if cv2.waitKey(1) & 0xFF == ord('q'):
+            #         stopped = True
+            #         break
+
+            if stopped:
+                break
+
+            # Skip frames
+            # for _ in range(frame_interval - 1):
+            #     cap.grab()
+    except Exception:
+        pass
+    finally:
+        print("releasing...")
+        cap.release()
+        cv2.destroyAllWindows()
